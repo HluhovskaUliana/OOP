7002fb3100737f923f771df9102d99200e2be173
HluhovskaUliana <uglugovska@gmail.com>
2025-09-20 23:09:52 +0300

    Add files via upload

---
+++ lab2/task4/task4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class task4 {
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        bool[] primes = new bool[number + 1];
+        for (int i = 0; i <= number; i++)
+        {
+            primes[i] = true;
+        }
+        
+        primes[0] = false;
+        primes[1] = false;
+
+        for (int p = 2; p * p <= number; p++)
+        {
+            if (primes[p])
+            {
+                for (int multiple = p * 2; multiple <= number; multiple += p)
+                {
+                    primes[multiple] = false;
+                }
+            }
+        }
+        
+        Console.WriteLine($"Prime numbers up to {number}");
+        for (int i = 2; i <= number; i++)
+        {
+            if (primes[i])
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
