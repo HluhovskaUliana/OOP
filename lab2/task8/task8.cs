86b5c39f71cf297e1550b6cbbe3108239c90512c
HluhovskaUliana <uglugovska@gmail.com>
2025-09-21 13:16:06 +0300

    1

---
+++ lab2/task8/task8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class task8
+{
+    static void Main()
+    {
+        Console.Write("Enter a sequence of numbers: ");
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int maxCount = 0;
+        int result = array[0];
+
+        foreach (int num in array.Distinct())
+        {
+            int count = array.Count(x => x == num);
+            if (count > maxCount || count == maxCount && Array.IndexOf(array, num) < Array.IndexOf(array, result))
+            {
+                maxCount = count;
+                result = num;
+            } 
+        }
+        Console.WriteLine($"Most frequent number: {result} ({maxCount})");
+        Console.ReadKey();
+    }
+}
---
+++ lab2/task9/task9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class task9
+{
+    static void Main()
+    {
+        char[] alphabet = new char[26];
+        for (int i = 0; i < 26; i++)
+        {
+            alphabet[i] = (char)('a' + i);
+        }
+        
+        Console.WriteLine("Enter a word: ");
+        string word = Console.ReadLine();
+
+        foreach (char latter in word)
+        {
+            int index = Array.IndexOf(alphabet, latter);
+            Console.WriteLine($"{latter} -> {index} ");
+        }
+        Console.ReadKey();
+    }
+}
