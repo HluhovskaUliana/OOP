2df5980424b2e5d565ecef4ee373cc09b1886ea5
HluhovskaUliana <uglugovska@gmail.com>
2025-10-08 23:36:52 +0300

    1

---
+++ lab7/task2/BirthdayCelebrations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BirthdayCelebrations
+{
+    static void Main()
+    {
+        List<string> detainedIds = new List<string>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] parts = input.Split(' ');
+            string id = parts[parts.Length - 1];
+            
+            detainedIds.Add(id);
+        }
+        
+        string fakeSuffix = Console.ReadLine();
+
+        foreach (string id in detainedIds)
+        {
+            if (id.EndsWith(fakeSuffix))
+            {
+                Console.WriteLine(id);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
