9512daa6d22fcb837aebcdb2282abfc1fc1956d0
HluhovskaUliana <uglugovska@gmail.com>
2025-10-26 20:51:50 +0200

    1

--- lab11/task1/HarvestingFieldsTest.cs
+++ lab11/task1/HarvestingFieldsTest.cs
@@ -1,13 +1,43 @@
 ﻿namespace P01_HarvestingFields
 {
     using System;
-    
+    using System.Reflection;
     public class HarvestingFieldsTest
     {
         public static void Main()
         {
-            
+            Type type = typeof(HarvestingFields);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "HARVEST")
+                    break;
+
+                FieldInfo[] fields = type.GetFields(
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                foreach (FieldInfo field in fields)
+                {
+                    string accessModifier = GetAccessModifier(field);
+
+                    if (command == "all" || command == accessModifier)
+                    {
+                        Console.WriteLine($"{accessModifier} {field.FieldType.Name} {field.Name}");
+                    }
+                }
+            }
         }
+        
+        static string GetAccessModifier(FieldInfo field)
+        {
+            if (field.IsPrivate) return "private";
+            if (field.IsPublic) return "public";
+            if (field.IsFamily) return "protected"; 
+            return "unknown";
+        }
+
     }
 }
 
---
+++ lab11/task2/BlackBoxIntegerTest.cs
@@ -0,0 +1,41 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+    
+    public class BlackBoxIntegerTest
+    {
+        public static void Main()
+        {
+            Type type = typeof(BlackBoxIntegerTest);
+            
+            ConstructorInfo ctor = type.GetConstructor(
+                BindingFlags.Instance | BindingFlags.NonPublic,
+                null, Type.EmptyTypes, null);
+
+            object instance = ctor.Invoke(null);
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] parts = input.Split('_');
+                string methodName = parts[0];
+                int value = int.Parse(parts[1]);
+                
+                MethodInfo method = type.GetMethod(
+                    methodName,
+                    BindingFlags.Instance | BindingFlags.NonPublic);
+
+                method.Invoke(instance, new object[] { value });
+
+                FieldInfo field = type.GetField(
+                    "innerValue",
+                    BindingFlags.Instance | BindingFlags.NonPublic);
+
+                Console.WriteLine(field.GetValue(instance));
+            }
+
+        }
+    }
+}
+
