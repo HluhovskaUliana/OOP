d8294aaa614fabbac1f19831c076281da8d1ee1b
HluhovskaUliana <uglugovska@gmail.com>
2025-11-03 00:51:53 +0200

    1

---
+++ lab12/task3/WorkForce.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkForce
+{
+    public interface IEmployee
+    {
+        string Name { get; }
+        int WorkHoursPerWeek { get; }
+    }
+
+    public class StandardEmployee : IEmployee
+    {
+        public string Name { get; private set; }
+        public int WorkHoursPerWeek => 40;
+
+        public StandardEmployee(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class PartTimeEmployee : IEmployee
+    {
+        public string Name { get; private set; }
+        public int WorkHoursPerWeek => 20;
+
+        public PartTimeEmployee(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Job
+    {
+        public string Name { get; private set; }
+        public int HoursRequired { get; private set; }
+        public IEmployee Employee { get; private set; }
+
+        public event EventHandler<Job> JobDone;
+
+        public Job(string name, int hoursRequired, IEmployee employee)
+        {
+            Name = name;
+            HoursRequired = hoursRequired;
+            Employee = employee;
+        }
+
+        public void Update()
+        {
+            HoursRequired -= Employee.WorkHoursPerWeek;
+            if (HoursRequired <= 0)
+            {
+                Console.WriteLine($"Job {Name} done!");
+                JobDone?.Invoke(this, this);
+            }
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"Job: {Name} Hours Remaining: {HoursRequired}");
+        }
+    }
+
+    public class JobList
+    {
+        private List<Job> jobs = new List<Job>();
+
+        public void Add(Job job)
+        {
+            job.JobDone += OnJobDone;
+            jobs.Add(job);
+        }
+
+        private void OnJobDone(object sender, Job job)
+        {
+            jobs.Remove(job);
+        }
+
+        public void PassWeek()
+        {
+            foreach (var job in new List<Job>(jobs))
+            {
+                job.Update();
+            }
+        }
+
+        public void PrintStatus()
+        {
+            foreach (var job in jobs)
+            {
+                job.Status();
+            }
+        }
+    }
+    
+    class WorkForce
+    {
+        static void Main()
+        {
+            Dictionary<string, IEmployee> employees = new Dictionary<string, IEmployee>();
+            JobList jobList = new JobList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] parts = command.Split();
+
+                if (parts[0] == "StandardEmployee")
+                {
+                    string name = parts[1];
+                    employees[name] = new StandardEmployee(name);
+                }
+                else if (parts[0] == "PartTimeEmployee")
+                {
+                    string name = parts[1];
+                    employees[name] = new PartTimeEmployee(name);
+                }
+                else if (parts[0] == "Job")
+                {
+                    string jobName = parts[1];
+                    int hours = int.Parse(parts[2]);
+                    string employeeName = parts[3];
+
+                    if (employees.ContainsKey(employeeName))
+                    {
+                        Job job = new Job(jobName, hours, employees[employeeName]);
+                        jobList.Add(job);
+                    }
+                }
+                else if (command == "Pass Week")
+                {
+                    jobList.PassWeek();
+                }
+                else if (command == "Status")
+                {
+                    jobList.PrintStatus();
+                }
+            }
+        }
+    }
+
+}
+
---
+++ lab12/task4/KingsGambitExtended.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KingsGambitExtended
+{
+    public interface IDefender
+    {
+        string Name { get; }
+        bool IsAlive { get; }
+        void RespondToAttack();
+        void TakeHit();
+        event EventHandler<string> OnDeath;
+    }
+
+    public class King
+    {
+        public string Name { get; private set; }
+        private List<IDefender> defenders;
+
+        public King(string name)
+        {
+            Name = name;
+            defenders = new List<IDefender>();
+        }
+
+        public void AddDefender(IDefender defender)
+        {
+            defender.OnDeath += HandleDeath;
+            defenders.Add(defender);
+        }
+
+        private void HandleDeath(object sender, string name)
+        {
+            defenders.RemoveAll(d => d.Name == name);
+        }
+
+        public void RespondToAttack()
+        {
+            Console.WriteLine($"King {Name} is under attack!");
+            foreach (var defender in defenders)
+            {
+                if (defender.IsAlive)
+                {
+                    defender.RespondToAttack();
+                }
+            }
+        }
+
+        public void HitDefender(string name)
+        {
+            var defender = defenders.Find(d => d.Name == name);
+            if (defender != null)
+            {
+                defender.TakeHit();
+            }
+        }
+    }
+
+    public class Footman : IDefender
+    {
+        public string Name { get; private set; }
+        private int hitsTaken = 0;
+        public bool IsAlive { get; private set; } = true;
+
+        public event EventHandler<string> OnDeath;
+
+        public Footman(string name)
+        {
+            Name = name;
+        }
+
+        public void RespondToAttack()
+        {
+            Console.WriteLine($"Footman {Name} is panicking!");
+        }
+
+        public void TakeHit()
+        {
+            hitsTaken++;
+            if (hitsTaken >= 2 && IsAlive)
+            {
+                IsAlive = false;
+                OnDeath?.Invoke(this, Name);
+            }
+        }
+    }
+
+    public class RoyalGuard : IDefender
+    {
+        public string Name { get; private set; }
+        private int hitsTaken = 0;
+        public bool IsAlive { get; private set; } = true;
+
+        public event EventHandler<string> OnDeath;
+
+        public RoyalGuard(string name)
+        {
+            Name = name;
+        }
+
+        public void RespondToAttack()
+        {
+            Console.WriteLine($"Royal Guard {Name} is defending!");
+        }
+
+        public void TakeHit()
+        {
+            hitsTaken++;
+            if (hitsTaken >= 3 && IsAlive)
+            {
+                IsAlive = false;
+                OnDeath?.Invoke(this, Name);
+            }
+        }
+    }
+
+    class KingsGambitExtended
+    {
+        static void Main()
+        {
+            string kingName = Console.ReadLine();
+            King king = new King(kingName);
+
+            string[] guards = Console.ReadLine().Split();
+            foreach (var guard in guards)
+            {
+                king.AddDefender(new RoyalGuard(guard));
+            }
+
+            string[] footmen = Console.ReadLine().Split();
+            foreach (var footman in footmen)
+            {
+                king.AddDefender(new Footman(footman));
+            }
+
+            string command;
+            int commandCount = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (commandCount >= 100) break;
+                commandCount++;
+
+                if (command == "Attack King")
+                {
+                    king.RespondToAttack();
+                }
+                else if (command.StartsWith("Kill "))
+                {
+                    string nameToKill = command.Substring(5);
+                    if (nameToKill == king.Name)
+                    {
+                        Console.WriteLine("You cannot kill the king!");
+                        continue;
+                    }
+
+                    king.HitDefender(nameToKill);
+                }
+            }
+        }
+    }
+}
