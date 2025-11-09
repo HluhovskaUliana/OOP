f8846863266e85e06318f0bb3eb56fa5cdf91977
HluhovskaUliana <uglugovska@gmail.com>
2025-11-09 19:09:39 +0200

    1

---
+++ lab13/task1/StudentByGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Group  { get; set; }
+}
+
+class StudentByGroup
+{
+    static void Main()
+    {
+        var students = new List<Student>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            var parts = input.Split(' ');
+            students.Add(new Student()
+            {
+                FirstName = parts[0], LastName = parts[1], Group = int.Parse(parts[2])
+            });
+        }
+        
+        var groupStudents = students.Where(s => s.Group == 2).OrderByDescending(s => s.FirstName);
+
+        foreach (var student in groupStudents)
+        {
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+        }
+
+        Console.ReadKey();
+    }
+}
