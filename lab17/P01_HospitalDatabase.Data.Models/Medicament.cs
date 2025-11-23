556fbf826ee3846871d6eebc4a1602d08e8a633a
HluhovskaUliana <uglugovska@gmail.com>
2025-11-23 18:56:39 +0200

    1

---
+++ lab17/P01_HospitalDatabase.Data.Models/Medicament.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_HospitalDatabase.Data.Models
+{
+    public class Medicament
+    {
+        public int MedicamentId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PatientMedicament> PatientMedicaments { get; set; } = new HashSet<PatientMedicament>();
+    }
+}
+
